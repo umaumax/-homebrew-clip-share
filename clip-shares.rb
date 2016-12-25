a81a08992784786f2e172dfe5f8bca0e6567affd
umaumax <umauma7083@gmail.com>
2016-12-25 20:27:24 +0900

    change

--- clip-shares.rb
+++ clip-shares.rb
@@ -11,7 +11,7 @@ resource "gobootstrap" do
 	url "https://github.com/umaumax/clip-share/archive/0.0.1.tar.gz"
 	sha256 "717e8045ec82b886767edf83f523ff260f72fcb4b217804d5dc244a61b5d70e1"
  #   url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
-    version "1.7"
+	version "0.0.1"
   #  sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
   end
 
