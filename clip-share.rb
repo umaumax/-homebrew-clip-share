5b7dbd27859e9cf5ee5744dd00e0a4ca3e960a7c
umaumax <umauma7083@gmail.com>
2016-12-25 19:04:56 +0900

    change files

--- clip-share.rb
+++ clip-share.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class ClipShares < Formula
+class ClipShare < Formula
   homepage "https://github.com/umaumax/clip-share"
   url "https://github.com/umaumax/clip-share/archive/0.0.1.tar.gz"
   sha256 "717e8045ec82b886767edf83f523ff260f72fcb4b217804d5dc244a61b5d70e1"
